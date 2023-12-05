8469c8e32f83638d03c89f67529d2f843ffb9f35
Cheng-K <adamkei2612@gmail.com>
2023-12-05 22:42:26 +0800

    Initial Commit

---
+++ src/test/java/com/chengk/springmvcmarketplace/SpringMvcMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengk.springmvcmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
