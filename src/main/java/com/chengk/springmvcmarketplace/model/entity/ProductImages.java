a168fe4631c2a23c4671cecaa06181a47c523c44
Cheng-K <adamkei2612@gmail.com>
2023-12-08 23:37:50 +0800

    Update entities

--- src/main/java/com/chengk/springmvcmarketplace/model/entity/ProductImages.java
+++ src/main/java/com/chengk/springmvcmarketplace/model/entity/ProductImages.java
@@ -6,6 +6,28 @@ import org.springframework.data.relational.core.mapping.Table;
 @Table("ProductImages")
 public class ProductImages {
     @Id
-    public Integer id;
-    public String path;
+    private Integer id;
+    private String path;
+
+    public ProductImages(Integer id, String path) {
+        this.id = id;
+        this.path = path;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
 }
--- src/main/java/com/chengk/springmvcmarketplace/model/entity/Products.java
+++ src/main/java/com/chengk/springmvcmarketplace/model/entity/Products.java
@@ -13,12 +13,80 @@ import com.chengk.springmvcmarketplace.model.value_objects.Condition;
 @Table("Products")
 public class Products {
     @Id
-    public Integer id;
-    public String title;
-    public String description;
-    public Categories category;
-    public Timestamp listedOn;
-    public Condition condition;
+    private Integer id;
+    private String title;
+    private String description;
+    private Categories category;
+    private Timestamp listedOn;
+    private Condition condition;
     @MappedCollection(idColumn = "product_id")
-    public Set<ProductImages> images = new HashSet<>();
+    private Set<ProductImages> images = new HashSet<>();
+
+    public Products(Integer id, String title, String description, Categories category, Timestamp listedOn,
+            Condition condition, Set<ProductImages> images) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.listedOn = listedOn;
+        this.condition = condition;
+        this.images = images;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Categories getCategory() {
+        return category;
+    }
+
+    public void setCategory(Categories category) {
+        this.category = category;
+    }
+
+    public Timestamp getListedOn() {
+        return listedOn;
+    }
+
+    public void setListedOn(Timestamp listedOn) {
+        this.listedOn = listedOn;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
+
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
+
+    public Set<ProductImages> getImages() {
+        return images;
+    }
+
+    public void setImages(Set<ProductImages> images) {
+        this.images = images;
+    }
+
 }
