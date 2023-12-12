d37a32325e7e570eac486ac115aafb6539701aee
Cheng-K <adamkei2612@gmail.com>
2023-12-12 23:54:03 +0800

    Update product service
    - Add method to add new product

--- src/main/java/com/chengk/springmvcmarketplace/domain/ProductsService.java
+++ src/main/java/com/chengk/springmvcmarketplace/domain/ProductsService.java
@@ -6,4 +6,6 @@ import com.chengk.springmvcmarketplace.model.dto.ProductDto;
 
 public interface ProductsService {
     public List<ProductDto> getAllProducts();
+
+    public void addNewProduct(ProductDto productDto);
 }
--- src/main/java/com/chengk/springmvcmarketplace/domain/ProductsServiceImpl.java
+++ src/main/java/com/chengk/springmvcmarketplace/domain/ProductsServiceImpl.java
@@ -1,7 +1,9 @@
 package com.chengk.springmvcmarketplace.domain;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -14,11 +16,13 @@ public class ProductsServiceImpl implements ProductsService {
 
     private ProductRepository productRepository;
     private DtoConverter<Products, ProductDto> productDtoConverter;
+    private StorageService storageService;
 
     public ProductsServiceImpl(ProductRepository productRepository,
-            DtoConverter<Products, ProductDto> productDtoConverter) {
+            DtoConverter<Products, ProductDto> productDtoConverter, StorageService storageService) {
         this.productRepository = productRepository;
         this.productDtoConverter = productDtoConverter;
+        this.storageService = storageService;
     }
 
     @Override
@@ -26,9 +30,25 @@ public class ProductsServiceImpl implements ProductsService {
         List<ProductDto> result = new ArrayList<>();
         Iterable<Products> products = productRepository.findAll();
         for (var product : products) {
-            result.add(productDtoConverter.convert(product));
+            result.add(productDtoConverter.convertToDto(product));
         }
         return result;
+    }
+
+    @Override
+    public void addNewProduct(ProductDto productDto) {
+        String originalFileName = productDto.getImageFile().getOriginalFilename();
+        String fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        String fileName = UUID.randomUUID().toString() + fileExtension;
+        try {
+            storageService.saveFile(productDto.getImageFile().getBytes(),
+                    fileName, "./src/main/resources/static/img");
+        } catch (IOException e) {
+            System.err.println("Cannot read the bytes of multipart file");
+            e.printStackTrace();
+        }
+        productDto.setImage(fileName);
+        productRepository.save(productDtoConverter.convertToEntity(productDto));
     }
 
 }
