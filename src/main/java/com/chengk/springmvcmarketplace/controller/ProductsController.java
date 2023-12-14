dd97dd783e54cd1866e337c53f3285fd331670ca
Cheng-K <adamkei2612@gmail.com>
2023-12-14 16:38:38 +0800

    Update ProductsController.java
    - Initialize necessary default fields with value

--- src/main/java/com/chengk/springmvcmarketplace/controller/ProductsController.java
+++ src/main/java/com/chengk/springmvcmarketplace/controller/ProductsController.java
@@ -1,9 +1,12 @@
 package com.chengk.springmvcmarketplace.controller;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +16,9 @@ import com.chengk.springmvcmarketplace.domain.CategoryService;
 import com.chengk.springmvcmarketplace.domain.ProductsService;
 import com.chengk.springmvcmarketplace.model.dto.CategoryDto;
 import com.chengk.springmvcmarketplace.model.dto.ProductDto;
+import com.chengk.springmvcmarketplace.model.value_objects.Condition;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/products")
@@ -35,14 +41,23 @@ public class ProductsController {
 
     @GetMapping("/add")
     public String getAddProductsForm(Model model) {
+        ProductDto newProduct = new ProductDto();
+        newProduct.setCondition(Condition.USED);
         List<CategoryDto> categories = categoryService.getAllCategories();
         model.addAttribute("availableCategories", categories);
-        model.addAttribute("newProduct", new ProductDto());
+        model.addAttribute("newProduct", newProduct);
         return "products-add";
     }
 
     @PostMapping("/add")
-    public String postAddProductsForm(@ModelAttribute("newProduct") ProductDto productDto) {
+    public String postAddProductsForm(@ModelAttribute("newProduct") @Valid ProductDto productDto,
+            BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            List<CategoryDto> categories = categoryService.getAllCategories();
+            model.addAttribute("availableCategories", categories);
+            return "products-add";
+        }
+        productDto.setListedOn(Timestamp.valueOf(LocalDateTime.now()));
         productsService.addNewProduct(productDto);
         return "redirect:/products";
     }
