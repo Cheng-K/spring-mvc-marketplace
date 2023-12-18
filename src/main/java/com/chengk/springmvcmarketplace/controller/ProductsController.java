88ffab81ef99dc8a0e239110248082b63335284d
Cheng-K <adamkei2612@gmail.com>
2023-12-18 17:45:38 +0800

    Update ProductsController.java
    - Add delete mapping for products
    - Add put mapping for updating products

--- src/main/java/com/chengk/springmvcmarketplace/controller/ProductsController.java
+++ src/main/java/com/chengk/springmvcmarketplace/controller/ProductsController.java
@@ -7,10 +7,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.chengk.springmvcmarketplace.domain.CategoryService;
@@ -53,23 +56,56 @@ public class ProductsController {
     @PostMapping("/add")
     public String postAddProductsForm(@ModelAttribute("newProduct") @Valid ProductDto productDto,
             BindingResult result, Model model) {
+        if (productDto.getImageFile() == null || productDto.getImageFile().isEmpty()) {
+            result.rejectValue("imageFile", "no.image.uploaded", "Product image must be uploaded.");
+        }
         if (result.hasErrors()) {
             List<CategoryDto> categories = categoryService.getAllCategories();
             model.addAttribute("availableCategories", categories);
             return "products-add";
         }
-        productDto.setListedOn(Timestamp.valueOf(LocalDateTime.now()));
+        productDto.setListedOn(LocalDateTime.now());
         productsService.addNewProduct(productDto);
         return "redirect:/products";
     }
 
     @GetMapping("/{productId}")
-    public String getMethodName(@PathVariable("productId") Integer productId, Model model) {
+    public String getProductDetails(@PathVariable("productId") Integer productId, Model model) {
         ProductDto productDto = productsService.getProductById(productId);
         if (productDto == null)
             return "404";
         model.addAttribute("product", productDto);
         return "products-detail";
+    }
+
+    @DeleteMapping("/{productId}")
+    public String deleteProduct(@PathVariable("productId") Integer productId) {
+        productsService.removeProduct(productId);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/{productId}/edit")
+    public String getEditProductForm(@PathVariable("productId") Integer productId, Model model) {
+        ProductDto productDto = productsService.getProductById(productId);
+        if (productDto == null)
+            return "404";
+        model.addAttribute("product", productDto);
+        List<CategoryDto> categories = categoryService.getAllCategories();
+        model.addAttribute("availableCategories", categories);
+        return "products-edit";
+    }
+
+    @PutMapping("/{productId}")
+    public String updateProductDetails(@PathVariable("productId") Integer productId,
+            @ModelAttribute("product") @Valid ProductDto productDto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            List<CategoryDto> categories = categoryService.getAllCategories();
+            model.addAttribute("availableCategories", categories);
+            return "products-edit";
+        }
+        productDto.setId(productId);
+        productsService.editProduct(productDto);
+        return String.format("redirect:/products/%d", productId);
     }
 
 }
