07082db32cb86b3ccb9007ddc97f83c9414036ae
Cheng-K <adamkei2612@gmail.com>
2023-12-18 14:52:43 +0800

    Update storage service
    - Add file name UUID generation
    - Add delete file functionality

--- src/main/java/com/chengk/springmvcmarketplace/domain/StorageService.java
+++ src/main/java/com/chengk/springmvcmarketplace/domain/StorageService.java
@@ -2,4 +2,8 @@ package com.chengk.springmvcmarketplace.domain;
 
 public interface StorageService {
     void saveFile(byte[] bytes, String fileName, String directoryPath);
+
+    void deleteFile(String filePath) throws Exception;
+
+    String replaceFileNameWithUUID(String fileName);
 }
--- src/main/java/com/chengk/springmvcmarketplace/domain/StorageServiceImpl.java
+++ src/main/java/com/chengk/springmvcmarketplace/domain/StorageServiceImpl.java
@@ -1,8 +1,10 @@
 package com.chengk.springmvcmarketplace.domain;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -22,7 +24,21 @@ public class StorageServiceImpl implements StorageService {
             System.err.println(e.getMessage());
             e.printStackTrace();
         }
+    }
 
+    @Override
+    public void deleteFile(String filePath) throws Exception {
+        File targetFile = new File(filePath);
+        if (!targetFile.delete()) {
+            throw new Exception("Unable to delete the file");
+        }
+    }
+
+    @Override
+    public String replaceFileNameWithUUID(String fileName) {
+        String fileExtension = fileName.substring(fileName.lastIndexOf("."));
+        String newFileName = UUID.randomUUID().toString() + fileExtension;
+        return newFileName;
     }
 
 }
