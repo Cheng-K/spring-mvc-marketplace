5bd1aa9d42f450933fc233c414184c46af78e0b1
Cheng-K <adamkei2612@gmail.com>
2023-12-27 14:48:22 +0800

    Update CategoriesController.java

--- src/main/java/com/chengk/springmvcmarketplace/controller/CategoriesController.java
+++ src/main/java/com/chengk/springmvcmarketplace/controller/CategoriesController.java
@@ -1,11 +1,19 @@
 package com.chengk.springmvcmarketplace.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.chengk.springmvcmarketplace.domain.CategoryService;
+import com.chengk.springmvcmarketplace.model.dto.CategoryDto;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/categories")
@@ -20,7 +28,29 @@ public class CategoriesController {
     @GetMapping
     public String getAllCategories(Model model) {
         model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("newCategory", new CategoryDto());
+        model.addAttribute("submittedCategory", new CategoryDto());
         return "categories-list";
+    }
+
+    @PostMapping
+    public String postNewCategory(@Valid @ModelAttribute("submittedCategory") CategoryDto categoryDto,
+            BindingResult result,
+            Model model) {
+
+        if (!categoryDto.getTitle().isEmpty() && categoryService.doesCategoryExists(categoryDto.getTitle())) {
+            result.rejectValue("title", "duplicate.category", "Category already exist.");
+        }
+        if (result.hasErrors()) {
+            model.addAttribute("categories", categoryService.getAllCategories());
+            model.addAttribute("newCategory", new CategoryDto());
+            model.addAttribute("hasErrors", true);
+            return "categories-list";
+        }
+        categoryDto.setCreatedOn(LocalDateTime.now());
+        categoryDto.setLastUpdated(LocalDateTime.now());
+        categoryService.addNewCategory(categoryDto);
+        return "redirect:/categories";
     }
 
 }
