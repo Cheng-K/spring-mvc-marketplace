f1433c79d15ab62d8d2e2673852e38d82ec7d4a8
Cheng-K <adamkei2612@gmail.com>
2024-01-23 21:16:20 +0800

    Add logging errors

--- src/main/java/com/chengk/springmvcmarketplace/controller/AppErrorController.java
+++ src/main/java/com/chengk/springmvcmarketplace/controller/AppErrorController.java
@@ -1,5 +1,7 @@
 package com.chengk.springmvcmarketplace.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,14 +12,18 @@ import com.chengk.springmvcmarketplace.model.dto.HttpErrorDto;
 
 @ControllerAdvice
 public class AppErrorController {
+    Logger logger = LoggerFactory.getLogger(AppErrorController.class);
+
     @ExceptionHandler({ AppResponseException.class })
     public String handleResponseStatusException(AppResponseException ex, Model model) {
+        logger.error(ex.getErrorDetails().getSubtitle(), ex);
         model.addAttribute("error", ex.getErrorDetails());
         return "http-error";
     }
 
     @ExceptionHandler({ Exception.class })
     public String handleGenericException(Exception ex, Model model) {
+        logger.error(ex.getMessage(), ex);
         model.addAttribute("error", new HttpErrorDto(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error occurred",
                 "An unexpected error is encountered. Please try again."));
         return "http-error";
